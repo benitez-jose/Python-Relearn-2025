5a0b5aa692ca00e4c2119bbbbdbf4e49b7b99abc
Jose Benitez <jbenitez86@live.com>
2025-06-17 13:46:50 -0700

    adding new folder and files

---
+++ Python Crash Course/Chapter 2/2_2_simple_messages.py
@@ -0,0 +1,7 @@
+message1 = "This is the first simple message."
+message2 = "This is the second simple message."
+message3 = "This is the third simple message."
+
+print(message1)
+print(message2)
+print(message3)
