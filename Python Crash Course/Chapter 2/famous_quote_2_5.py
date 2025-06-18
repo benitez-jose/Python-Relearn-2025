1861582650f030eb7ca8a06edfa190245ec270f7
Jose Benitez <jbenitez86@live.com>
2025-06-17 22:26:32 -0700

    more files added

---
+++ Python Crash Course/Chapter 2/famous_quote_2_5.py
@@ -0,0 +1,9 @@
+# Find a quote from a famous person you admire. Print the quote and the name of its author.
+# Your output should look like example including the quotation marks.
+
+first_name = "Albert"
+last_name = "Einstein"
+
+quote = "A person who never made a mistake never tried anything new."
+
+print(f'{first_name} {last_name} once said, "{quote}"')
