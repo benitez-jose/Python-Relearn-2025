a5bcff2d2da79137315470efe6c5dd6f77476ae6
Jose Benitez <jbenitez86@live.com>
2025-06-19 06:43:47 -0700

    added the 1st four exercises base files for chapter 3

---
+++ Python Crash Course/Chapter 3/greetings.py
@@ -0,0 +1,4 @@
+# Try 3-2: Start with a list you used in Exercise 3-1, but instead of just
+# printing each person's name, print a message to them. The text of each 
+# message should be the same, but each message should be personalized with
+# the person's name.
